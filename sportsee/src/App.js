eb91b7bfc430308d8ae52048ce9dfed296c1f25c
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-12-07 16:10:22 +0100

    feat: ajout de la barre de navigation vertical

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,10 +1,15 @@
 import Header from './layouts/header'
+import VerticalBar from './layouts/verticalBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <VerticalBar />   
+      </main>
+
     </div>
   );
 }

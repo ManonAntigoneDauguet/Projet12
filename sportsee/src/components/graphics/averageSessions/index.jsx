9dc25375ee7ea21bba0c329fab40c1c79ba8c24c
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-12-14 11:58:45 +0100

    feat: installation de Recharts. feat: ajout d'une base au graph1.

--- sportsee/src/components/graphics/averageSessions/index.jsx
+++ sportsee/src/components/graphics/averageSessions/index.jsx
@@ -1,13 +1,14 @@
 import style from "./averageSessions.module.css"
 import { getAverageSessions } from "../../../services/callsAPI"
 import { useState, useEffect } from "react"
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 
 function AverageSessionsGraph({ userId, isMockedData }) {
     const [isLoadingGet, updateIsLoadingGet] = useState(true)
     const [isError, updateIsError] = useState(false)
     const [averageSessions, updateData] = useState()
-
+    
     const getInformations = async() => {
         const averageSessionsData = await getAverageSessions(userId, isMockedData) 
         if (typeof averageSessionsData === "object") { 
@@ -22,18 +23,95 @@ function AverageSessionsGraph({ userId, isMockedData }) {
         getInformations()
     }, [])
 
-    return (
-        <div className={ style.graph__content }>
-            { !isLoadingGet && !isError &&
-                <div>
-                    <h2>Average Graph</h2>    
-                    { averageSessions.sessions.map(({ day, sessionLength }) => (
-                        <p key={ day }>{ sessionLength }</p>
-                    ))}
+    const renderCustomAxisTick = ({ x, y, payload }) => {
+        let dayName = ""
+        switch (payload.value) {
+            case 1 : 
+                dayName = "L"
+                break
+            case 2 : 
+                dayName = "M"
+                break
+            case 3 : 
+                dayName = "M"
+                break
+            case 4 : 
+                dayName = "J"
+                break
+            case 5 : 
+                dayName = "V"
+                break
+            case 6 : 
+                dayName = "S"
+                break
+            case 7 : 
+                dayName = "D"
+                break
+            default :
+                dayName = ""
+        }
+        return (
+        <text x={x+18} y={y+16} dy={-40}>
+            {dayName}
+        </text>
+        )
+    }
+
+    const renderCustomTooltip = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            return (
+                <div className={ style.tooltip }>
+                    <span>{`${payload[0].value } min`}</span>
                 </div>
+            )            
+        }
+        return null
+    }
+
+    return (
+        <div className={ style.graphContainer }>
+            <h2 className={ style.title }>Durée moyenne des sessions</h2>
+            { !isLoadingGet && !isError &&
+
+                    <LineChart 
+                        title="" 
+                        width={ 270 } 
+                        height={ 263 } 
+                        data={ averageSessions.sessions } 
+                        className={ style.graph }
+                    >
+                        <Line 
+                            type="monotone" 
+                            dataKey="sessionLength" 
+                            stroke="#fff" 
+                        />
+                        <XAxis 
+                            dataKey="day" 
+                            height={1} 
+                            axisLine={false} 
+                            tickLine={false} 
+                            tick={ renderCustomAxisTick } 
+                            className={ style.XAxis }
+                        />
+                        <YAxis 
+                            dataKey="sessionLength" 
+                            type="number" 
+                            domain={['dataMin-15', 'dataMax+20']} 
+                            hide 
+                        />
+                        <Tooltip 
+                            content={ renderCustomTooltip }  
+                            
+                        />
+                    </LineChart>
+                
             }
-            { isLoadingGet && !isError && <p>En chargement...</p> }
-            { !isLoadingGet && isError && <p>Utilisateur introuvable...</p> }
+            { isLoadingGet && !isError
+                && <p className={ style.errorMessage }>En chargement...</p> 
+            }
+            { !isLoadingGet && isError 
+                && <p className={ style.errorMessage }>Erreur de chargement des données...</p> 
+            }
         </div>
     )
 }

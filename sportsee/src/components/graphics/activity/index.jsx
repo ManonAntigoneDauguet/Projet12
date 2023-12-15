5dfc4757e0e77614da4d9ca08605eb6d24fb99ae
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-12-15 12:01:04 +0100

    feat: ajout d'une base au graphique "Activity".

--- sportsee/src/components/graphics/activity/index.jsx
+++ sportsee/src/components/graphics/activity/index.jsx
@@ -1,6 +1,7 @@
 import style from "./activity.module.css"
 import { getActivity } from "../../../services/callsAPI"
 import { useState, useEffect } from "react"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceArea, BarChart, Bar, Rectangle, CartesianAxis } from 'recharts'
 
 
 function ActivityGraph({ userId, isMockedData }) {
@@ -23,17 +24,55 @@ function ActivityGraph({ userId, isMockedData }) {
     }, [])
 
     return (
-        <div className={ style.graph__content }>
+        <div className={ style.graphContainer }>
+            <h2 className={ style.title }>Activity Graph</h2>
             { !isLoadingGet && !isError &&
-                <div>
-                    <h2>Activity Graph</h2>    
-                    { activity.map(({ day }) => (
-                        <p key={ day }>{ day }</p>
-                    ))}
-                </div>
+                <ResponsiveContainer height="70%" width="100%">
+                    <BarChart
+                        data={ activity }
+                        className={ style.graph }
+                    >
+                        <CartesianGrid 
+                            strokeDasharray="2 2" 
+                            horizontal={ true }
+                            vertical={ false }
+                        />
+                        <XAxis 
+                            dataKey="day"
+                            tickLine={ false } 
+                            axisLine={{ stroke: 'gray' }}
+                            className={ style.XAxis }
+                        />
+                        <YAxis 
+                            dataKey="kilogram"
+                            axisLine={ false } 
+                            tickLine={ false } 
+                            orientation="right"
+                            type="number"
+                            // tickCount={ 4 }
+                            interval="equidistantPreserveStartEnd"
+                            domain={['dataMin-20', 'dataMax']}
+                            className={ style.YAxis }
+                        />
+                        <Legend 
+                            verticalAlign="top"
+                            align="right"
+                        />
+                        <Bar 
+                            dataKey="kilogram" 
+                            fill="rgba(40, 45, 48, 1)"
+                        />
+                        <Bar 
+                            dataKey="calories" 
+                            fill="rgba(255, 0, 0, 1)"
+                        />
+                    </BarChart>
+                </ResponsiveContainer>
             }
-            { isLoadingGet && !isError && <p>En chargement...</p> }
-            { !isLoadingGet && isError && <p>Utilisateur introuvable...</p> }
+            { isLoadingGet && !isError 
+                && <p className={ style.errorMessage }>En chargement...</p> }
+            { !isLoadingGet && isError 
+                && <p className={ style.errorMessage }>Erreur de chargement des données...</p> }
         </div>
     )
 }

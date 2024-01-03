b9189a28295261ebe477b1b277c47326cdaf4c84
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2024-01-03 09:59:04 +0100

    refactor: ajout d'un tableau de correspondance dans les iconCards pour les couleurs et les styles CSS associés.

--- sportsee/src/components/iconCard/index.jsx
+++ sportsee/src/components/iconCard/index.jsx
@@ -5,37 +5,28 @@ import style from "./iconCard.module.css"
  * Return a image as a anchor or a card
  * @param { String } img
  * @param { String } alt
- * @param { String } [color]
+ * @param { String } [color="white"]
  * @param { String } [navLink] as the path of Router
  * @returns { HTMLElement }
  */
-function IconCard({ img, alt, color, navLink }) {
-    let colorClass
-    switch ( color ) {
-        case 'red':
-            colorClass = ( style.red )
-            break
-        case 'blue':
-            colorClass = ( style.blue )
-            break
-        case 'yellow':
-            colorClass = ( style.yellow )
-            break
-        case 'pink':
-            colorClass = ( style.pink )
-            break
-        default:
-            colorClass = ( style.white )
-    }
+function IconCard({ img, alt, color = "white", navLink }) {
+    let colorClass = 
+        {
+            "red": style.red,
+            "blue": style.blue,
+            "yellow": style.yellow,
+            "pink": style.pink,
+            "white": style.white
+        }
 
     return (
         navLink !== undefined ? 
             <a
                 href={ navLink }
-                className={ `${style.iconCard} ${colorClass}` }>
+                className={ `${style.iconCard} ${colorClass[color]}` }>
                 <img src={ img } alt={ alt } />
             </a> 
-            : <div className={ `${style.iconCard} ${colorClass}` }>
+            : <div className={ `${style.iconCard} ${colorClass[color]}` }>
                 <img src={ img } alt={ alt } />
             </div>
     )
